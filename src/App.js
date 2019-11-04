96e8fdca707410f27c018c6f7eb4dc41f67a7f27
Jonas Forsberg <jonas.forsberg@gmail.com>
2019-11-04 15:00:40 +0100

    Testing useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = React.useState('Hello React');
+
+  const handleChange = event => {
+    setText(event.target.value);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{text}</p>
+      <input type="text" value={text} onChange={handleChange} />
+    </>
   );
-}
+};
 
 export default App;
